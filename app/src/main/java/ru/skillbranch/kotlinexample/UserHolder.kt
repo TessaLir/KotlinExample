376119a1a6727a79de2f96f443bb6b5b80545d42
vvn <finik001@gmail.com>
2020-07-18 22:32:45 +0300

    Создан проект, пройден урок

---
+++ app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
@@ -0,0 +1,28 @@
+package ru.skillbranch.kotlinexample
+
+import androidx.annotation.VisibleForTesting
+
+object UserHolder {
+    private val map = mutableMapOf<String, User>()
+
+    fun refisterUser(
+        fullName: String,
+        email: String,
+        password: String
+    ): User = User.makeUser(fullName, email = email, password = password)
+        .also { user -> map[user.login] = user }
+
+    fun loginUser(
+        login: String,
+        password: String
+    ): String? = map[login.trim()]?.let {
+        if (it.checkPassword(password)) it.userInfo
+        else null
+    }
+
+    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
+    fun clearHolder() {
+        map.clear()
+    }
+
+}
